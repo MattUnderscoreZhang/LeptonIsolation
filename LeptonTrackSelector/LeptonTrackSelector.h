2df7dfc4f890d6ee662dab8a3f82e57017067486
Matt Zhang <set.stun@gmail.com>
2018-02-22 21:16:30 +0100

    Merged lepton track selection into SusySkim2LJetsLegacy, and moved obsolete files back here for storage

---
+++ LeptonTrackSelector/LeptonTrackSelector.h
@@ -0,0 +1,24 @@
+#ifndef SusySkim2LJetsLegacy_LeptonTrackSelector_h
+#define SusySkim2LJetsLegacy_LeptonTrackSelector_h
+
+//RootCore
+#include "SusySkimMaker/BaseUser.h"
+
+class LeptonTrackSelector : public BaseUser
+{
+
+ public:
+  LeptonTrackSelector();
+  ~LeptonTrackSelector() {};
+
+  void setup(ConfigMgr*& configMgr);
+  bool doAnalysis(ConfigMgr*& configMgr);
+  bool passCuts(ConfigMgr*& configMgr);
+  void finalize(ConfigMgr*& configMgr);
+
+
+};
+
+static const BaseUser* LeptonTrackSelector_instance __attribute__((used)) = new LeptonTrackSelector();
+
+#endif

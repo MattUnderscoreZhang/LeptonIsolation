de55cf0220d74c835fd1ada02a698fc4f1c223da
Matt Zhang <set.stun@gmail.com>
2018-02-23 22:50:10 -0700

    Made h5 conversion script able to handle vectors (and store all variable names in one place)

--- ConvertROOTToH5/convert_to_h5.py
+++ ConvertROOTToH5/convert_to_h5.py
@@ -30,50 +30,65 @@ def convertFile(inFile, outFile):
     input_file = ROOT.TFile.Open(str(inFile), "read")
     myFeatures = FeaturesList()
 
+    all_features = [
+        ("lep_q", "int"),
+        ("lep_pt", "float"),
+        ("lep_eta", "float"),
+        ("lep_phi", "float"),
+        ("lep_m", "float"),
+        ("lep_d0", "float"),
+        ("lep_z0", "float"),
+        ("lep_d0Err", "float"),
+        ("lep_z0Err", "float"),
+        ("lep_pTErr", "float"),
+        ("lep_ptcone20", "float"),
+        ("lep_ptcone30", "float"),
+        ("lep_ptcone40", "float"),
+        ("lep_topoetcone20", "float"),
+        ("lep_topoetcone30", "float"),
+        ("lep_topoetcone40", "float"),
+        ("lep_ptvarcone20", "float"),
+        ("lep_ptvarcone30", "float"),
+        ("lep_ptvarcone40", "float"),
+        ("lep_truthAuthor", "int"),
+        ("lep_truthType", "int"),
+        ("lep_truthOrigin", "int"),
+        ("track_q", "float"),
+        ("track_pt", "float"),
+        ("track_eta", "float"),
+        ("track_phi", "float"),
+        ("track_m", "float"),
+        ("track_fitQuality", "float"),
+        ("track_d0", "float"),
+        ("track_z0", "float"),
+        ("track_d0Err", "float"),
+        ("track_z0Err", "float"),
+        ("track_nIBLHits", "int"),
+        ("track_nPixHits", "int"),
+        ("track_nPixHoles", "int"),
+        ("track_nPixOutliers", "int"),
+        ("track_nSCTHits", "int"),
+        ("track_nTRTHits", "int")]
+
     for event in input_file.tree_NoSys:
-        myFeatures.add("lep_q", event.lep_q)
-        myFeatures.add("lep_pt", event.lep_pt)
-        myFeatures.add("lep_eta", event.lep_eta)
-        myFeatures.add("lep_phi", event.lep_phi)
-        myFeatures.add("lep_m", event.lep_m)
-        myFeatures.add("lep_d0", event.lep_d0)
-        myFeatures.add("lep_z0", event.lep_z0)
-        myFeatures.add("lep_d0Err", event.lep_d0Err)
-        myFeatures.add("lep_z0Err", event.lep_z0Err)
-        myFeatures.add("lep_pTErr", event.lep_pTErr)
-        myFeatures.add("lep_ptcone20", event.lep_ptcone20)
-        myFeatures.add("lep_ptcone30", event.lep_ptcone30)
-        myFeatures.add("lep_ptcone40", event.lep_ptcone40)
-        myFeatures.add("lep_topoetcone20", event.lep_topoetcone20)
-        myFeatures.add("lep_topoetcone30", event.lep_topoetcone30)
-        myFeatures.add("lep_topoetcone40", event.lep_topoetcone40)
-        myFeatures.add("lep_ptvarcone20", event.lep_ptvarcone20)
-        myFeatures.add("lep_ptvarcone30", event.lep_ptvarcone30)
-        myFeatures.add("lep_ptvarcone40", event.lep_ptvarcone40)
-        myFeatures.add("lep_truthAuthor", event.lep_truthAuthor)
-        myFeatures.add("lep_truthType", event.lep_truthType)
-        myFeatures.add("lep_truthOrigin", event.lep_truthOrigin)
-        myFeatures.add("track_q", event.track_q)
-        myFeatures.add("track_pt", event.track_pt)
-        myFeatures.add("track_eta", event.track_eta)
-        myFeatures.add("track_phi", event.track_phi)
-        myFeatures.add("track_m", event.track_m)
-        myFeatures.add("track_fitQuality", event.track_fitQuality)
-        myFeatures.add("track_d0", event.track_d0)
-        myFeatures.add("track_z0", event.track_z0)
-        myFeatures.add("track_d0Err", event.track_d0Err)
-        myFeatures.add("track_z0Err", event.track_z0Err)
-        myFeatures.add("track_nIBLHits", event.track_nIBLHits)
-        myFeatures.add("track_nPixHits", event.track_nPixHits)
-        myFeatures.add("track_nPixHoles", event.track_nPixHoles)
-        myFeatures.add("track_nPixOutliers", event.track_nPixOutliers)
-        myFeatures.add("track_nSCTHits", event.track_nSCTHits)
-        myFeatures.add("track_nTRTHits", event.track_nTRTHits)
+        for (feature, _) in all_features:
+            exec("event_feature = event." + feature)
+            value = []
+            for i in range(event_feature.size()):
+                value.append(event_feature[i])
+            myFeatures.add(feature, value)
 
     # Save features to an h5 file
     f = h5.File(outFile, "w")
-    for key in myFeatures.keys():
-        f.create_dataset(key, data=np.array(myFeatures.get(key)).squeeze(),compression='gzip')
+    for (feature, data_type) in all_features:
+        feature_data = myFeatures.get(feature)
+        if data_type == "int":
+            dt = h5.special_dtype(vlen=np.dtype('int32'))
+        elif data_type == "float":
+            dt = h5.special_dtype(vlen=np.dtype('float32'))
+        new_data = f.create_dataset(feature, (len(feature_data),), dtype=dt)
+        for i, data in enumerate(feature_data):
+            new_data[i] = data
     f.close()
     
 #################

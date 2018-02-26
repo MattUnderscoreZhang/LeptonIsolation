db30c41594e185da83658fdd2e115a300a243976
Matt Zhang <set.stun@gmail.com>
2018-02-26 12:28:43 -0700

    Now store lepton variables as well, and keep feature names in a separate file for use by comparison scripts

--- Scripts/convert.py
+++ Scripts/convert.py
@@ -1,7 +1,10 @@
+import sys
+sys.dont_write_bytecode=True
 import numpy as np
 import sys, ast
 import ROOT
 import pickle
+from features import *
 
 ############
 # Features #
@@ -29,53 +32,12 @@ def convertFile(inFile, outFile):
 
     input_file = ROOT.TFile.Open(str(inFile), "read")
 
-    lep_features = [
-        ("lep_q", "int"),
-        ("lep_pt", "float"),
-        ("lep_eta", "float"),
-        ("lep_phi", "float"),
-        ("lep_m", "float"),
-        ("lep_d0", "float"),
-        ("lep_z0", "float"),
-        ("lep_d0Err", "float"),
-        ("lep_z0Err", "float"),
-        ("lep_pTErr", "float"),
-        ("lep_ptcone20", "float"),
-        ("lep_ptcone30", "float"),
-        ("lep_ptcone40", "float"),
-        ("lep_topoetcone20", "float"),
-        ("lep_topoetcone30", "float"),
-        ("lep_topoetcone40", "float"),
-        ("lep_ptvarcone20", "float"),
-        ("lep_ptvarcone30", "float"),
-        ("lep_ptvarcone40", "float"),
-        ("lep_truthAuthor", "int"),
-        ("lep_truthType", "int"),
-        ("lep_truthOrigin", "int")]
-
-    track_features = [
-        ("track_q", "float"),
-        ("track_pt", "float"),
-        ("track_eta", "float"),
-        ("track_phi", "float"),
-        ("track_m", "float"),
-        ("track_fitQuality", "float"),
-        ("track_d0", "float"),
-        ("track_z0", "float"),
-        ("track_d0Err", "float"),
-        ("track_z0Err", "float"),
-        ("track_nIBLHits", "int"),
-        ("track_nPixHits", "int"),
-        ("track_nPixHoles", "int"),
-        ("track_nPixOutliers", "int"),
-        ("track_nSCTHits", "int"),
-        ("track_nTRTHits", "int")]
-
-    all_features = lep_features + track_features
-
     max_dR = 0.4
 
-    lepton_track_associations = []
+    all_data = []
+    # [event][lepton]
+    # each lepton has [lep_var1, lep_var2, ... [track container]]
+    # each track has [track_var1, track_var2, ...]
     for event in input_file.tree_NoSys:
         # convert ROOT vectors into arrays
         event_features = FeaturesList()
@@ -85,19 +47,19 @@ def convertFile(inFile, outFile):
             for i in range(event_feature_rootvec.size()):
                 event_feature.append(event_feature_rootvec[i])
             event_features.add(feature_name, event_feature)
-        # get associated tracks for each lepton
+        # get info for each lepton in event
         lep_etas = event_features.get('lep_eta')
         lep_phis = event_features.get('lep_phi')
         track_etas = event_features.get('track_eta')
         track_phis = event_features.get('track_phi')
-        event_lepton_track_associations = [] # [event][lepton][track][feature]
-        for lep_data in zip(lep_etas, lep_phis):
-            lep_eta = lep_data[0]
-            lep_phi = lep_data[1]
+        event_data = []
+        for i, (lep_eta, lep_phi) in enumerate(zip(lep_etas, lep_phis)):
+            lepton_data = []
+            for (feature_name, _) in lep_features:
+                lepton_data.append(event_features.get(feature_name)[i])
             associated_tracks = []
-            for j, track_data in enumerate(zip(track_etas, track_phis)):
-                track_eta = track_data[0]
-                track_phi = track_data[1]
+            # get associated tracks for each lepton
+            for j, (track_eta, track_phi) in enumerate(zip(track_etas, track_phis)):
                 dEta = pow(lep_eta - track_eta, 2)
                 dPhi = abs(lep_phi - track_phi) % (2*np.pi)
                 if dPhi > np.pi:
@@ -110,13 +72,15 @@ def convertFile(inFile, outFile):
                         associated_track.append(event_features.get(feature_name)[j])
                     associated_tracks.append(associated_track)
             if len(associated_tracks) > 0:
-                event_lepton_track_associations.append(associated_tracks)
-        if len(event_lepton_track_associations) > 0:
-            lepton_track_associations.append(event_lepton_track_associations)
+                lepton_data.append(associated_tracks)
+                event_data.append(lepton_data)
+        if len(event_data) > 0:
+            all_data.append(event_data)
 
     # Save features to a pickle file
     with open(outFile, 'w') as f:
-        pickle.dump(lepton_track_associations, f)
+        # should save as dictionary with lepton info too
+        pickle.dump(all_data, f)
     
 #################
 # Main function #
@@ -124,7 +88,11 @@ def convertFile(inFile, outFile):
 
 if __name__ == "__main__":
     inFile = "/afs/cern.ch/work/m/mazhang/LepIso/Ntuples/393407.root"
-    outFile = "/afs/cern.ch/work/m/mazhang/LepIso/H5/393407.pkl"
+    # inFiles = [
+        # "/afs/cern.ch/work/m/mazhang/LepIso/Ntuples/393407.root",
+        # "/afs/cern.ch/work/m/mazhang/LepIso/Ntuples/410472.root"
+        # ]
+    outFile = "/afs/cern.ch/work/m/mazhang/LepIso/Pkl/393407.pkl"
     print "Converting file"
     convertFile(inFile, outFile)
     print "Finished"

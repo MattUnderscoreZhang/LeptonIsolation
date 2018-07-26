5946c6ddc0930c70e365282368038b7614e80fbc
Matt Zhang <set.stun@gmail.com>
2018-07-26 18:06:36 +0200

    Added script for running on multiple MUON5 files

---
+++ ROOTToH5/make_all.sh
@@ -0,0 +1,29 @@
+./x86_64-slc6-gcc62-opt/bin/dump-xaod /eos/user/m/mazhang/LepIso/MC/MUON5/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_MUON5.e6337_e5984_s3126_r10201_r10210_p3584/DAOD_MUON5.14537525._000001.pool.root.1
+mv output.h5 Outputs/output_1.h5
+
+./x86_64-slc6-gcc62-opt/bin/dump-xaod /eos/user/m/mazhang/LepIso/MC/MUON5/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_MUON5.e6337_e5984_s3126_r10201_r10210_p3584/DAOD_MUON5.14537525._000002.pool.root.1
+mv output.h5 Outputs/output_2.h5
+
+./x86_64-slc6-gcc62-opt/bin/dump-xaod /eos/user/m/mazhang/LepIso/MC/MUON5/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_MUON5.e6337_e5984_s3126_r10201_r10210_p3584/DAOD_MUON5.14537525._000003.pool.root.1
+mv output.h5 Outputs/output_3.h5
+
+./x86_64-slc6-gcc62-opt/bin/dump-xaod /eos/user/m/mazhang/LepIso/MC/MUON5/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_MUON5.e6337_e5984_s3126_r10201_r10210_p3584/DAOD_MUON5.14537525._000004.pool.root.1
+mv output.h5 Outputs/output_4.h5
+
+./x86_64-slc6-gcc62-opt/bin/dump-xaod /eos/user/m/mazhang/LepIso/MC/MUON5/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_MUON5.e6337_e5984_s3126_r10201_r10210_p3584/DAOD_MUON5.14537525._000005.pool.root.1
+mv output.h5 Outputs/output_5.h5
+
+./x86_64-slc6-gcc62-opt/bin/dump-xaod /eos/user/m/mazhang/LepIso/MC/MUON5/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_MUON5.e6337_e5984_s3126_r10201_r10210_p3584/DAOD_MUON5.14537525._000006.pool.root.1
+mv output.h5 Outputs/output_6.h5
+
+./x86_64-slc6-gcc62-opt/bin/dump-xaod /eos/user/m/mazhang/LepIso/MC/MUON5/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_MUON5.e6337_e5984_s3126_r10201_r10210_p3584/DAOD_MUON5.14537525._000007.pool.root.1
+mv output.h5 Outputs/output_7.h5
+
+./x86_64-slc6-gcc62-opt/bin/dump-xaod /eos/user/m/mazhang/LepIso/MC/MUON5/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_MUON5.e6337_e5984_s3126_r10201_r10210_p3584/DAOD_MUON5.14537525._000008.pool.root.1
+mv output.h5 Outputs/output_8.h5
+
+./x86_64-slc6-gcc62-opt/bin/dump-xaod /eos/user/m/mazhang/LepIso/MC/MUON5/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_MUON5.e6337_e5984_s3126_r10201_r10210_p3584/DAOD_MUON5.14537525._000009.pool.root.1
+mv output.h5 Outputs/output_9.h5
+
+./x86_64-slc6-gcc62-opt/bin/dump-xaod /eos/user/m/mazhang/LepIso/MC/MUON5/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_MUON5.e6337_e5984_s3126_r10201_r10210_p3584/DAOD_MUON5.14537525._000010.pool.root.1
+mv output.h5 Outputs/output_10.h5

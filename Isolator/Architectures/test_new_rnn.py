7e5854a8c2c2706414473201db97d8f288b2b051
ParticularlyPythonicBS <anilr2@illinois.edu>
2018-09-23 17:59:30 -0500

    clean up

--- Isolator/Architectures/test_new_rnn.py
+++ Isolator/Architectures/test_new_rnn.py
@@ -14,7 +14,7 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.n_layers = options["n_layers"]
         self.size = options["n_size"]
-        self.batch_size=options["batch_size"]
+        self.batch_size = options["batch_size"]
         self.rnn = nn.RNN(
             input_size=self.size[0], hidden_size=self.size[1], num_layers=self.n_layers)
         self.fc = nn.Linear(self.size[1], self.size[2])
@@ -29,14 +29,14 @@ class RNN(nn.Module):
     def forward(self, track,):
 
         track = track.view(1, track.size()[0])
-        rnn_input=pack_padded_sequence(
+        rnn_input = pack_padded_sequence(
             track, self.batch_size)
-
+        print(rnn_input)
+        hidden=self._init_hidden(self.batch_size)
         self.rnn.flatten_parameters()
-        # output, hidden = self.rnn(x, hidden)
+        output, hidden = self.rnn(rnn_input, hidden)
 
-
-        return [0,0]
+        return output, hidden
 
     def accuracy(self, output, truth):
         _, top_i = output.data.topk(1)
@@ -57,22 +57,15 @@ class RNN(nn.Module):
             truth, lepton, tracks = event
             for track in tracks:
                 output, hidden = self.forward(track)
-        #     # total_loss += self.loss_function(output, truth)
-        #     # total_acc += self.accuracy(output, truth)
-        #     raw_results.append(output.detach().numpy()[0][0])
-        #     all_truth.append(truth.detach().numpy()[0])
-        # total_loss /= len(events)
-        # total_acc = total_acc.float() / len(events)
-        # if do_training:
-        #     total_loss.backward()
-        #     for param in self.parameters():
-        #         param.data.add_(-self.learning_rate, param.grad.data)
-        # return total_loss.data.item(), total_acc.data.item(), raw_results, all_truth
-
-
-    def arg_print(self):
-
-        print(vars(self))
-
-
+            total_loss += self.loss_function(output, truth)
+            total_acc += self.accuracy(output, truth)
+            raw_results.append(output.detach().numpy()[0][0])
+            all_truth.append(truth.detach().numpy()[0])
+        total_loss /= len(events)
+        total_acc = total_acc.float() / len(events)
+        if do_training:
+            total_loss.backward()
+            for param in self.parameters():
+                param.data.add_(-self.learning_rate, param.grad.data)
+        return total_loss.data.item(), total_acc.data.item(), raw_results, all_truth
 

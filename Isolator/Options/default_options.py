403866cc1761371c67ba9804905b5bfc80b7e456
Matt Zhang <set.stun@gmail.com>
2018-09-24 12:31:43 -0500

    Added options files

---
+++ Isolator/Options/default_options.py
@@ -0,0 +1,7 @@
+options = {}
+options['n_hidden_output_neurons'] = 8
+options['n_hidden_middle_neurons'] = 8
+options['learning_rate'] = 0.01
+options['training_split'] = 0.9
+options['batch_size'] = 200
+options['n_batches'] = 5000

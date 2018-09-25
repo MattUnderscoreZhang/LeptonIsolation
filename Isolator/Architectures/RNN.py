786bb76e712af16529267ff1104163f2c9f28605
ParticularlyPythonicBS <anilr2@illinois.edu>
2018-09-24 19:20:48 -0500

    testing works! learning rate is not a thing yet

--- Isolator/Architectures/RNN.py
+++ Isolator/Architectures/RNN.py
@@ -57,6 +57,7 @@ class RNN(nn.Module):
             hidden = torch.zeros(1, self.n_hidden_output_neurons)
             truth, lepton, tracks = event
             # print(event)
+            pdb.set_trace()
             for track in tracks:
                 output, hidden = self.forward(track, hidden)
             total_loss += self.loss_function(output, truth)
--- Isolator/Architectures/test_new_rnn.py
+++ Isolator/Architectures/test_new_rnn.py
@@ -3,12 +3,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pack_sequence
-
+import pdb
 
 class RNN(nn.Module):
-    """docstring for RNN"""
+    """RNN module implementing pytorch rnn"""
 
     def __init__(self, options):
         super(RNN, self).__init__()
@@ -18,7 +17,7 @@ class RNN(nn.Module):
         self.batch_size = options["batch_size"]
         self.learning_rate = options['learning_rate']
         self.rnn = nn.RNN(
-            input_size=self.size[0], hidden_size=self.size[1],batch_first=True,
+            input_size=self.size[0], hidden_size=self.size[1], batch_first=True,
             num_layers=self.n_layers, bidirectional=options["bidirectional"])
         self.fc = nn.Linear(self.size[1], self.size[2])
         self.loss_function = nn.CrossEntropyLoss()
@@ -33,10 +32,11 @@ class RNN(nn.Module):
 
         self.rnn.flatten_parameters()
         output, hidden = self.rnn(pack_sequence(track))
-
-        return output, hidden
+        fc_output = self.fc(hidden[-1])
+        return fc_output
 
     def accuracy(self, output, truth):
+
         _, top_i = output.data.topk(1)
         category = top_i[0][0]
         return (category == truth.data[0])
@@ -51,18 +51,22 @@ class RNN(nn.Module):
         total_acc = 0
         raw_results = []
         all_truth = []
-        for event in events:
-            truth, lepton, tracks = event
-            for track in tracks:
-                output, hidden = self.forward(track)
-            total_loss += self.loss_function(output, truth)
+        # pdb.set_trace()
+        for i, data in enumerate(loader, 1):
+
+            output = self.forward(data[0])
+            truth = data[1]
+            loss=self.loss_function(output.data, torch.max(truth, 1)[1])
+            total_loss+=loss.data.item()
             total_acc += self.accuracy(output, truth)
-            raw_results.append(output.detach().numpy()[0][0])
+            raw_results.append(output.data.detach().numpy()[0][0])
             all_truth.append(truth.detach().numpy()[0])
-        total_loss /= len(events)
-        total_acc = total_acc.float() / len(events)
-        if do_training:
-            total_loss.backward()
-            for param in self.parameters():
-                param.data.add_(-self.learning_rate, param.grad.data)
+        total_loss /= len(loader.dataset)
+        total_acc = total_acc.float() / len(loader.dataset)
+        total_loss=torch.tensor(total_loss)
+        total_acc=torch.tensor(total_acc)
+        # if do_training:
+            # total_loss.backward()
+            # for param in self.parameters():
+            #     param.data.add_(-self.learning_rate, param.grad.data)
         return total_loss.data.item(), total_acc.data.item(), raw_results, all_truth

Bug fix to loss calculation

Isolator/Architectures/RNN.py
@@ -21,9 +21,9 @@ class RNN(nn.Module):
         self.n_directions = int(options["bidirectional"]) + 1
         self.n_layers = options["n_layers"]
         self.input_size = options["track_size"]
-        self.hidden_size = options["hidden_size"]
+        self.hidden_size = options["hidden_neurons"]
         self.lepton_size = options["lepton_size"]
-        self.output_size = options["output_size"]
+        self.output_size = options["output_neurons"]
         self.batch_size = options["batch_size"]
         self.learning_rate = options['learning_rate']
         if options['RNN_type'] is 'vanilla':
@@ -93,7 +93,7 @@ class RNN(nn.Module):
                                        truth.data.detach()[indices])
             raw_results += list(output[:,0].data.detach().numpy())
             all_truth += list(truth.detach()[indices].numpy())
-        total_loss /= len(events.dataset)
+        total_loss = total_loss / len(events.dataset) * self.batch_size
         total_acc = total_acc / len(events.dataset) * self.batch_size
         total_loss = torch.tensor(total_loss)
         total_acc = torch.tensor(total_acc)
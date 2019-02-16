3fa744c2e4d5d9f45825c59742137cc071c98f06
Matt Zhang <set.stun@gmail.com>
2019-02-16 00:10:49 -0600

    Slight changes to ptcone plotting

--- Analyzer/compare_ptcone.py
+++ Analyzer/compare_ptcone.py
@@ -9,18 +9,18 @@ def compare(options):
     data_file = options['input_data']
     leptons_with_tracks = pkl.load(open(data_file, 'rb'))
 
-    # make ptcone and etcone comparison plots - normed
-    plot_save_dir = "../Plots_normed/"
-    pathlib.Path(plot_save_dir).mkdir(parents=True, exist_ok=True)
-    lwt = list(zip(
-        leptons_with_tracks['normed_leptons'],
-        leptons_with_tracks['normed_tracks']))
-    labels = [leptons_with_tracks['lepton_labels'],
-              leptons_with_tracks['track_labels']]
-    FeatureComparer.compare_ptcone_and_etcone(lwt, labels, plot_save_dir)
+    # # make ptcone and etcone comparison plots - normed
+    # plot_save_dir = "Plots_normed/"
+    # pathlib.Path(plot_save_dir).mkdir(parents=True, exist_ok=True)
+    # lwt = list(zip(
+        # leptons_with_tracks['normed_leptons'],
+        # leptons_with_tracks['normed_tracks']))
+    # labels = [leptons_with_tracks['lepton_labels'],
+              # leptons_with_tracks['track_labels']]
+    # FeatureComparer.compare_ptcone_and_etcone(lwt, labels, plot_save_dir, normed=True)
 
     # make ptcone and etcone comparison plots - unnormed
-    plot_save_dir = "../Plots_unnormed/"
+    plot_save_dir = "Plots_unnormed/"
     pathlib.Path(plot_save_dir).mkdir(parents=True, exist_ok=True)
     lwt = list(zip(
         leptons_with_tracks['unnormed_leptons'],

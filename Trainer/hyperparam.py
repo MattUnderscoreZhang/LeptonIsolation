f3bb6d01c890c64b76d6be4e317d9b75a7aa4324
ParticularlyPythonicBS <anilr2@illinois.edu>
2019-02-26 11:18:04 -0600

    framework for implementing hyperparameter tuning

--- Trainer/hyperparam.py
+++ Trainer/hyperparam.py
@@ -1,78 +1,84 @@
-import skopt.plots as skplot
-import skopt
-import numpy as np
-import pdb
-from isolator import RNN_Trainer
-import Loader.loader as loader
+import argparse
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+from ray.tune import Trainable
 
 
-from skopt.space import Integer, Categorical, Real
-from skopt.utils import use_named_args
-from skopt import gp_minimize
+# Training setting
 
-options = {}
-lwt = []
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument(
+    '--batch-size',
+    type=int,
+    default=64,
+    metavar='N',
+    help='input batch size for training (default: 64)')
+parser.add_argument(
+    '--test-batch-size',
+    type=int,
+    default=1000,
+    metavar='N',
+    help='input batch size for testing (default: 1000)')
+parser.add_argument(
+    '--epochs',
+    type=int,
+    default=10,
+    metavar='N',
+    help='number of epochs to train (default: 10)')
+parser.add_argument(
+    '--lr',
+    type=float,
+    default=0.01,
+    metavar='LR',
+    help='learning rate (default: 0.01)')
+parser.add_argument(
+    '--momentum',
+    type=float,
+    default=0.5,
+    metavar='M',
+    help='SGD momentum (default: 0.5)')
+parser.add_argument(
+    '--no-cuda',
+    action='store_true',
+    default=False,
+    help='disables CUDA training')
+parser.add_argument(
+    '--seed',
+    type=int,
+    default=1,
+    metavar='S',
+    help='random seed (default: 1)')
+parser.add_argument(
+    '--smoke-test', action="store_true", help="Finish quickly for testing")
 
-# set up hyperparameter space
-space = [Integer(16, 256, name='hidden_neurons'),
-         Integer(8, 256, name='n_layers'),
-         Real(0.00001, 1.0, name='learning_rate'),
-         ]
+
+class TrainRNN(Trainable):
+    def _setup(self, config):
+        continue
+
+    def _train_iteration(self):
+        continue
+
+    def _test(self):
+        continue
+
+    def _train(self):
+        self._train_iteration()
+        return self._test()
+
+    def _save(self, checkpoint_dir):
+        checkpoint_path = os.path.join(checkpoint_dir, "model.pth")
+        torch.save(self.model.state_dict(), checkpoint_path)
+        return checkpoint_path
+
+    def _restore(self, checkpoint_path):
+        self.model.load_state_dict(checkpoint_path)
 
 
-def option_maker(N_hidden, NL, LR):
-    '''Creates dictionary with required hyperparameters'''
-    options = {}
-    options["RNN_type"] = 'GRU'
-    options['learning_rate'] = LR
-    options['training_split'] = 0.7
-    options['batch_size'] = 100
-    options['n_batches'] = 50
-    options['n_layers'] = NL
-    options['hidden_neurons'] = N_hidden
-    options['output_neurons'] = 2
-    options['bidirectional'] = False
-    return options
-
-
-@use_named_args(space)
-def minimizer(**params):
-
-    global options,lwt
-
-    options['learning_rate'] = params['learning_rate']
-    options['n_layers'] = params['n_layers']
-    options['hidden_neurons'] = params['hidden_neurons']
-    trainer = RNN_Trainer(options, lwt)
-    loss = trainer.train_and_test()
-
-    return loss
-
-
-if __name__ == "__main__":
-
-    # # prepare data
-    in_file = "../Data/output.h5"
-    save_file = "../Data/lepton_track_data.pkl"
-    leptons_with_tracks = loader.create_or_load(
-        in_file, save_file, overwrite=False, pseudodata=False)
-
-    lwt = list(zip(leptons_with_tracks['normed_leptons'],
-                   leptons_with_tracks['normed_tracks']))
-
-    good_leptons = [i[leptons_with_tracks['lepton_labels'].index(
-        'ptcone20')] > 0 for i in leptons_with_tracks['unnormed_leptons']]
-    lwt = np.array(lwt)[good_leptons]
-
-    options = option_maker(128, 5, 0.0001)
-    # pdb.set_trace()
-    options['lepton_size'] = len(leptons_with_tracks['lepton_labels'])
-    options['track_size'] = len(leptons_with_tracks['track_labels'])
-
-    reg_gp = gp_minimize(minimizer, space, verbose=True, n_jobs=-1)
-
-    print('best score: {}'.format(reg_gp.fun))
-    print('best params:')
-    print('hidden_neurons: {}'.format(reg_gp.x[0]))
-    print('n_layers: {}'.format(reg_gp.x[1]))
-    print('learning_rate: {}'.format(reg_gp.x[2]))
+    

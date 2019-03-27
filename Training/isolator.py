82785872f180dcbadab49ba19fb0cac9cc08aed5
Matt Zhang <set.stun@gmail.com>
2019-03-27 16:36:45 -0500

    Fixed two bugs (truth info going to RNN, imbalanced classes)

--- Training/isolator.py
+++ Training/isolator.py
@@ -20,14 +20,14 @@ if __name__ == "__main__":
     '''python isolator.py'''
     options = {}
 
-    options["input_data"] = "/public/data/RNN/lepton_track_data.pkl"
+    options["input_data"] = "/public/data/RNN/lepton_track_data_track_pt_not_weighted.pkl"
     options["output_folder"] = "../Outputs/Runs/Test/"
 
     options["RNN_type"] = "RNN"
     options['learning_rate'] = 0.0005
     options['training_split'] = 0.7
-    options['batch_size'] = 200
-    options['n_epochs'] = 75
+    options['batch_size'] = 500
+    options['n_epochs'] = 50
     options['n_layers'] = 3
     options['hidden_neurons'] = 256
     options['output_neurons'] = 2

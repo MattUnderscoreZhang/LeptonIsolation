5eefa5630da8c3e7567648af6455a3525503f0a8
Matt Zhang <set.stun@gmail.com>
2019-06-24 21:53:59 -0500

    Added etcone variables to plotting script

--- MiscScripts/ptcone_comparison.py
+++ MiscScripts/ptcone_comparison.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 data = h5.File("output.h5")
 
-plot_names = ["ptcone20", "ptcone30", "ptcone40", "ptvarcone20", "ptvarcone30", "ptvarcone40"]
+plot_names = ["ptcone20", "ptcone30", "ptcone40", "ptvarcone20", "ptvarcone30", "ptvarcone40", "topoetcone20", "topoetcone30", "topoetcone40"]
 for n, plot_name in enumerate(plot_names):
     ref_ptcone = [i[8+n] for j in data['muons'] for i in j if not np.isnan(i[8+n])]
     calc_ptcone = [i[14+n] for j in data['muons'] for i in j if not np.isnan(i[14+n])]
@@ -15,7 +15,7 @@ for n, plot_name in enumerate(plot_names):
     plt.savefig("muon_" + plot_name + "_comparison.png")
     plt.clf()
 
-plot_names = ["ptcone20", "ptvarcone20", "ptvarcone30", "ptvarcone40"]
+plot_names = ["ptcone20", "ptvarcone20", "ptvarcone30", "ptvarcone40", "topoetcone20", "topoetcone40"]
 for n, plot_name in enumerate(plot_names):
     ref_ptcone = [i[8+n] for j in data['electrons'] for i in j if not np.isnan(i[8+n])]
     calc_ptcone = [i[12+n] for j in data['electrons'] for i in j if not np.isnan(i[12+n])]

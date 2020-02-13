9eab7965d0b52c1fcf2bcfd58f9ba885f186a76d
ParticularlyPythonicBS <anilr2@illinois.edu>
2020-02-13 16:46:06 -0600

    pre-commit test

--- Training/isolator.py
+++ Training/isolator.py
@@ -16,7 +16,11 @@ torch.backends.cudnn.enabled = True
 
 parser = argparse.ArgumentParser(description="Trainer")
 parser.add_argument("--disable-cuda", action="store_true", help="Disable CUDA")
-parser.add_argument("--continue-training", action="store_true", help="Loads in previous model and continues training")
+parser.add_argument(
+    "--continue-training",
+    action="store_true",
+    help="Loads in previous model and continues training",
+)
 args = parser.parse_args()
 args.device = None
 if not args.disable_cuda and torch.cuda.is_available():
@@ -32,23 +36,35 @@ if __name__ == "__main__":
 
     options["input_data"] = "/public/data/RNN/large_data.root"
     options["run_location"] = "/public/data/RNN/runs"
-    options["run_label"] = 'anil_set_test'
+    options["run_label"] = "anil_set_test"
     options["tree_name"] = "NormalizedTree"
     options["output_folder"] = "./Outputs/"
     options["model_path"] = options["output_folder"] + "saved_model.pt"
     options["continue_training"] = args.continue_training
-    options["architecture_type"] = "SetTransformer" # RNN, LSTM, GRU, DeepSets, SetTransformer
+    options[
+        "architecture_type"
+    ] = "SetTransformer"  # RNN, LSTM, GRU, DeepSets, SetTransformer
     options["dropout"] = 0.3
-    options["track_ordering"] = "low-to-high-pt"  # None, "high-to-low-pt", "low-to-high-pt", "near-to-far", "far-to-near"
+    options[
+        "track_ordering"
+    ] = "low-to-high-pt"  # None, "high-to-low-pt", "low-to-high-pt", "near-to-far", "far-to-near"
     # options["additional_appended_features"] = ["baseline_topoetcone20", "baseline_topoetcone30", "baseline_topoetcone40", "baseline_eflowcone20", "baseline_ptcone20", "baseline_ptcone30", "baseline_ptcone40", "baseline_ptvarcone20", "baseline_ptvarcone30", "baseline_ptvarcone40"]
     options["additional_appended_features"] = []
     options["lr"] = 0.001
-    options["ignore_features"] = ["baseline_topoetcone20", "baseline_topoetcone30",
-                                  "baseline_topoetcone40", "baseline_eflowcone20",
-                                  "baseline_ptcone20", "baseline_ptcone30",
-                                  "baseline_ptcone40", "baseline_ptvarcone20",
-                                  "baseline_ptvarcone30", "baseline_ptvarcone40",
-                                  "baseline_eflowcone20_over_pt", "trk_vtx_type"]
+    options["ignore_features"] = [
+        "baseline_topoetcone20",
+        "baseline_topoetcone30",
+        "baseline_topoetcone40",
+        "baseline_eflowcone20",
+        "baseline_ptcone20",
+        "baseline_ptcone30",
+        "baseline_ptcone40",
+        "baseline_ptvarcone20",
+        "baseline_ptvarcone30",
+        "baseline_ptvarcone40",
+        "baseline_eflowcone20_over_pt",
+        "trk_vtx_type",
+    ]
     options["training_split"] = 0.7
     options["batch_size"] = 200
     options["n_epochs"] = 30
@@ -60,7 +76,7 @@ if __name__ == "__main__":
 
     t0 = time.time()
     print("number of epochs planned:", options["n_epochs"])
-    print("input data:", options["input_data"].split('/')[-1])
+    print("input data:", options["input_data"].split("/")[-1])
     print("batch_size:", options["batch_size"])
     print("device:", args.device)
     print("architecture:", options["architecture_type"])
